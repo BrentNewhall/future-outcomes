9f0cc48248b0135b6a627bccf4cc8841e2371454
Brent P. Newhall <brent@brentnewhall.com>
2019-03-20 18:10:47 -0400

    Remove extra T in Help

--- src/components/Help.js
+++ src/components/Help.js
@@ -17,7 +17,7 @@ class Help extends Component {
               <h2>Playing Your Character</h2>
               <p>As you play, you will come across situations where one of your Moves applies. In that case, tap a Move's name and 2 possible Outcomes will be highlighted. Choose which one occurs by tapping that Outcome. If you tap a negative Outcome, you will get 1 point of Luck.</p>
               <p>Some Outcomes will apply a new Condition to your character, like <em>immobilized</em> or <em>distracted</em>. To add a Condition, tap the Add Condition button and select it.</p>
-              <p>There's one special Outcome: <em>hurt</em>. You can be hurt multiple times! There's a progress bar to track your hurt; tap the + or - buttons to add or remove hurt.</p>T
+              <p>There's one special Outcome: <em>hurt</em>. You can be hurt multiple times! There's a progress bar to track your hurt; tap the + or - buttons to add or remove hurt.</p>
             </div>
           </div>
         )
